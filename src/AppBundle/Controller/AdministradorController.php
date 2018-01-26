ec8c9fa7f8f28800370fe8350fa50bfda3fa397e
Manuel Carrillo <mdj.carleo@gmail.com>
2018-01-25 17:59:49 -0800

    Administrador controller

---
+++ src/AppBundle/Controller/AdministradorController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdministradorController extends Controller
+{
+	/**
+     * @Route("/admin/login", name="admin_login")
+     */
+    public function loginAction()
+    {
+    	return new Response("Hola Admin");
+    }
+}
