a74073567e96c87e629f1c2916fd5da9d4d98169
Manuel Carrillo <mdj.carleo@gmail.com>
2018-01-27 19:42:40 -0800

    modificaciones controller

--- src/AppBundle/Controller/AdministradorController.php
+++ src/AppBundle/Controller/AdministradorController.php
@@ -22,6 +22,23 @@ class AdministradorController extends Controller
     public function homeAction()
     {
     	$name = "Manuel";
-    	return $this->render('admin/admin_home.html.twig', array('name' => $name));
+    	return $this->render('admin/home.html.twig', array('name' => $name));
+    }
+
+    /**
+     * @Route("/admin/asesores", name="asesores")
+     */
+    public function asesoresAction()
+    {
+    	$periodo = "2018-1";
+    	$a1 = array(
+    	    "nombre" => "Manuel",
+    	    "apaterno" => "Carrillo",
+    	    "amaterno" => "Leon",
+    	);
+
+    	$a1 = ["Manuel","Carrillo","Leon"];
+    	$asesores = [$a1];
+    	return $this->render('admin/asesores.html.twig');
     }
 }
