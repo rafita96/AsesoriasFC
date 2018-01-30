f09c9b8972a3e88d75faf3db415fefad26f90791
rafita96 <rafael.peralta.blanco@gmail.com>
2018-01-29 19:31:59 -0800

    Horario seleccionable

--- src/AppBundle/Form/CitaType.php
+++ src/AppBundle/Form/CitaType.php
@@ -5,9 +5,9 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class CitaType extends AbstractType
 {
@@ -16,7 +16,27 @@ class CitaType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('tema')->add('materia')->add('expiracion')->add('cantidad')
+        $builder->add('tema')
+                ->add('materia')
+                ->add('expiracion', ChoiceType::class, array(
+                    "label" => "Expiración",
+                    "choices" => array(
+                        "Esta Semana" => "hoy",
+                        "Siguiente Semana" => "semana",
+                        "Este Mes" => "mes"
+                        ),
+                    "attr" => array(
+                        "class" => "form-control"
+                        )
+                    )
+                )
+                ->add('cantidad', null, array(
+                        'label' => 'Cantidad de alumnos',
+                        'attr' => array(
+                            "value" => 1,
+                        ),
+                    )
+                )
                 ->add('horario', HiddenType::class);
     }/**
      * {@inheritdoc}
