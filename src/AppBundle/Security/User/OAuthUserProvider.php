02d9b48aaa9cd73319b64586772c426ff9a7ff20
rafita96 <rafael.peralta.blanco@gmail.com>
2018-02-02 21:57:53 -0800

    Provider actualizado

--- src/AppBundle/Security/User/OAuthUserProvider.php
+++ src/AppBundle/Security/User/OAuthUserProvider.php
@@ -21,37 +21,77 @@ class OAuthUserProvider extends BaseClass
      */
     public function loadUserByOAuthUserResponse(UserResponseInterface $response)
     {
-        $socialID = $response->getUsername();
-        $repository = $this->userManager->getRepository(Alumno::class);
-        $user = $repository->findOneBy(array($this->getProperty($response)=>$socialID));
-        // $user = $this->userManager->findUserBy(array($this->getProperty($response)=>$socialID));
-        $email = $response->getEmail();
-        $pattern = "/(uabc.edu.mx)$/";
+        // $socialID = $response->getUsername();
+        // $repository = $this->userManager->getRepository(Alumno::class);
+        // $user = $repository->findOneBy(array($this->getProperty($response)=>$socialID));
+        // // $user = $this->userManager->findUserBy(array($this->getProperty($response)=>$socialID));
+        // $email = $response->getEmail();
+        // $pattern = "/(uabc.edu.mx)$/";
 
-        if (is_null($socialID)){
-            $this->container->get('security.context')->setToken(null);
+        // if (is_null($socialID)){
+        //     $this->container->get('security.context')->setToken(null);
+        //     return null;
+        // }
+        // if(null === $user){
+        //     if(preg_match($pattern, $email)){
+        //         $user = new Alumno();
+        //         $user->setEmail($email);
+        //         $user->setPlainPassword(md5(uniqid()));
+        //         $user->setEnabled(true);
+
+        //         $service = $response->getResourceOwner()->getName();
+        //         switch ($service) {
+        //             case 'google':
+        //                 $user->setGoogleID($socialID);
+        //                 break;
+        //         }
+
+        //         $this->userManager->persist($user);
+        //         $this->userManager->flush();
+        //     }
+        // }else{
+        //     $checker = new UserChecker();
+        //     $checker->checkPreAuth($user);   
+        // }
+ 
+        // return $user;
+
+        $socialID = $response->getUsername();
+        $user = $this->userManager->findUserBy(array($this->getProperty($response)=>$socialID));
+
+        $pattern = "/(uabc.edu.mx)$/";
+        $email = $response->getEmail();
+
+        if(preg_match($pattern, $email)){
             return null;
         }
-        if(null === $user){
-            if(preg_match($pattern, $email)){
-                $user = new Alumno();
+        //check if the user already has the corresponding social account
+        if (null === $user) {
+            //check if the user has a normal account
+            $user = $this->userManager->findUserByEmail($email);
+ 
+            if (null === $user || !$user instanceof UserInterface) {
+                //if the user does not have a normal account, set it up:
+                $user = $this->userManager->createUser();
                 $user->setEmail($email);
                 $user->setPlainPassword(md5(uniqid()));
                 $user->setEnabled(true);
-
-                $service = $response->getResourceOwner()->getName();
-                switch ($service) {
-                    case 'google':
-                        $user->setGoogleID($socialID);
-                        break;
-                }
-
-                $this->userManager->persist($user);
-                $this->userManager->flush();
             }
-        }else{
+            //then set its corresponding social id
+            $service = $response->getResourceOwner()->getName();
+            switch ($service) {
+                case 'google':
+                    $user->setGoogleID($socialID);
+                    break;
+                case 'facebook':
+                    $user->setFacebookID($socialID);
+                    break;
+            }
+            $this->userManager->updateUser($user);
+        } else {
+            //and then login the user
             $checker = new UserChecker();
-            $checker->checkPreAuth($user);   
+            $checker->checkPreAuth($user);
         }
  
         return $user;
